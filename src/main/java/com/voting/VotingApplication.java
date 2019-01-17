3f3b3152517332bc12101615d1c9ed656fd95b27
Vladimir Gorbatenko <mail.gva@gmail.com>
2019-01-17 16:05:15 +0200

    change secure configuration to multi config

--- src/main/java/com/voting/VotingApplication.java
+++ src/main/java/com/voting/VotingApplication.java
@@ -1,27 +1,14 @@
 package com.voting;
 
-import com.voting.configuration.MethodSecurityConfig;
-import com.voting.configuration.PersistenceJPAConfig;
-import com.voting.configuration.SecurityConfig;
-import com.voting.configuration.WebMvcConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableCaching
 @EnableWebMvc
-@EntityScan(basePackages = "com.voting.model")
-
 @SpringBootApplication
 public class VotingApplication extends SpringBootServletInitializer {
 	@Override
--- src/main/java/com/voting/configuration/SecurityConfig.java
+++ src/main/java/com/voting/configuration/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -22,66 +23,81 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-//@ComponentScan("com.voting.service.impl*")
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+
+    @Configuration
+    @Order(1)
+    public class RestConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring()
+                    .antMatchers("/resources/**","/webjars/**");
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(authenticationProvider());
+        }
+
+        public DaoAuthenticationProvider authenticationProvider() {
+            DaoAuthenticationProvider authProvider
+                    = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userService);
+            authProvider.setPasswordEncoder(passwordEncoder);
+            return authProvider;
+        }
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                    .httpBasic().and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .requestMatchers()
+                    .antMatchers("/rest/**")
+                    .and().authorizeRequests()
+                    .antMatchers("/rest/admin/**").hasRole("ADMIN")
+                    .antMatchers("/rest/profile/register").anonymous()
+                    .antMatchers("/**").authenticated()
+                    .and().csrf().disable();
+
+        }
+
+    }
+
+    @Configuration
+    @Order(2)
+    public class FormConf extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                    .authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .antMatchers("/register").anonymous()
+                    .antMatchers("/**/admin/**").hasRole("ADMIN")
+                    .antMatchers("/**").authenticated()
+                    .and().authorizeRequests()
+                    .anyRequest()
+                    .authenticated().and().formLogin()
+                    .loginPage("/login").defaultSuccessUrl("/voting")
+                    .failureUrl("/login?error=true")
+                    .loginProcessingUrl("/spring_security_check")
+                    .and().logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login").and().exceptionHandling()
+                    .and().csrf();
+        }
+    }
+
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserServiceImpl userService;
-
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/resources/**","/webjars/**");
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider
-                = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
-    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/register").anonymous()
-                .antMatchers("/**/admin/**").hasRole("ADMIN")
-                .antMatchers("/**").authenticated();
-
-
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated().and().formLogin()//csrf().disable().
-                .loginPage("/login").defaultSuccessUrl("/voting")
-                .failureUrl("/login?error=true")
-                .loginProcessingUrl("/spring_security_check")
-                .and().logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").and().exceptionHandling();
-
-
-        http.authorizeRequests()
-                .antMatchers("/rest/admin/**").hasRole("ADMIN").and().httpBasic().and().csrf().disable();
-
-        http.authorizeRequests()
-                .antMatchers("/rest/profile/register").anonymous().and().csrf().disable();
-
-
-
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
