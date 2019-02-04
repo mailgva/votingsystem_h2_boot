d8a7156037ff54e7eae35b001bb7584871ee915f
Vladimir Gorbatenko <mail.gva@gmail.com>
2019-02-04 10:22:40 +0200

    webjars

--- src/main/java/com/voting/configuration/WebMvcConfig.java
+++ src/main/java/com/voting/configuration/WebMvcConfig.java
@@ -93,6 +93,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/resources/")
                 .addResourceLocations("/webjars/","classpath:/META-INF/resources/webjars/");
 
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+
         registry.addResourceHandler("/pictures/**")
                 .addResourceLocations(IMAGES_PATH);
     }
