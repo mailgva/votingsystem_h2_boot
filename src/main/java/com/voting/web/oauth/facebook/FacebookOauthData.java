c262bab990990d1c72aad8c966a0873e74e68839
Vladimir Gorbatenko <mail.gva@gmail.com>
2019-02-05 09:39:34 +0200

    oauth replace http to https

--- src/main/java/com/voting/web/oauth/facebook/FacebookOauthData.java
+++ src/main/java/com/voting/web/oauth/facebook/FacebookOauthData.java
@@ -6,6 +6,6 @@ public class FacebookOauthData {
     protected static final String GET_LOGIN_URL = "https://graph.facebook.com/me?fields=name,email";
     protected static final String CLIENT_ID = "753232188383949";
     protected static final String CLIENT_SECRET = "720db3afe09d6fb5cd7ed0b9c409a600";
-    protected static final String REDIRECT_URI = "http://gva-votingsystem-boot.herokuapp.com/oauth/facebook/callback";
+    protected static final String REDIRECT_URI = "https://gva-votingsystem-boot.herokuapp.com/oauth/facebook/callback";
     protected static final String CODE = "voting_csrf_token_auth";
 }
--- src/main/java/com/voting/web/oauth/github/GitHubOauthData.java
+++ src/main/java/com/voting/web/oauth/github/GitHubOauthData.java
@@ -7,6 +7,6 @@ public class GitHubOauthData {
     protected static final String GET_LOGIN_URL = "https://api.github.com/user";
     protected static final String CLIENT_ID = "124edda838cddd271608";
     protected static final String CLIENT_SECRET = "564a9626b21b810f29932b50ce979336f270a89a";
-    protected static final String REDIRECT_URI = "http://gva-votingsystem-boot.herokuapp.com/oauth/github/callback";
+    protected static final String REDIRECT_URI = "https://gva-votingsystem-boot.herokuapp.com/oauth/github/callback";
     protected static final String CODE = "voting_csrf_token_auth";
 }
