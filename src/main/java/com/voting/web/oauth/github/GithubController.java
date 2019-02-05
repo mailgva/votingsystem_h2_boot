386fad90fdda60eac2364b73b11d67cc725ca610
Vladimir Gorbatenko <mail.gva@gmail.com>
2019-02-05 11:31:18 +0200

    some change

--- src/main/java/com/voting/web/oauth/github/GithubController.java
+++ src/main/java/com/voting/web/oauth/github/GithubController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 
 import static com.voting.web.oauth.github.GitHubOauthData.*;
 
@@ -16,15 +15,14 @@ import static com.voting.web.oauth.github.GitHubOauthData.*;
 public class GithubController extends AbstractOauthController {
 
     @RequestMapping("/authorize")
-    public String authorize(HttpServletRequest request) {
-        //String callbackUrl = request.getRequestURL().substring(0,request.getRequestURL().lastIndexOf("/")+1) + "callback";
+    public String authorize() {
         return "redirect:" + AUTHORIZE_URL + "?client_id=" + CLIENT_ID +
                 "&client_secret=" + CLIENT_SECRET + "&redirect_uri=" + REDIRECT_URI + "&state=" + CODE;
     }
 
     @RequestMapping("/callback")
-    public ModelAndView authenticate(@RequestParam String code, @RequestParam String state, RedirectAttributes attr, HttpServletRequest request) {
-        return super.authenticate(code, state, attr, request, ACCESS_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, GET_LOGIN_URL, REDIRECT_URI);
+    public ModelAndView authenticate(@RequestParam String code, @RequestParam String state, RedirectAttributes attr) {
+        return super.authenticate(code, state, attr, ACCESS_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, GET_LOGIN_URL, REDIRECT_URI);
     }
 
 }
